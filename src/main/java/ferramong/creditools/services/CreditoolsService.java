696b2c167dc15348b54c8a2424ad399cb27c514b
William Niemiec <williamniemiec@hotmail.com>
2021-04-18 01:02:59 -0300

    Services

---
+++ src/main/java/ferramong/creditools/services/CreditoolsService.java
@@ -0,0 +1,42 @@
+package ferramong.creditools.services;
+
+import ferramong.creditools.repositories.CreditoolsRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.ws.rs.NotFoundException;
+
+@Service
+@AllArgsConstructor
+public class CreditoolsService {
+
+    private final CreditoolsRepository repository;
+
+    public void creditUsingMoney(int idDweller, double value) {
+        if (!repository.existsById(idDweller))
+            throw new NotFoundException();
+
+        repository.creditDweller(idDweller, value);
+    }
+
+    public void creditUsingCreditools(int idDweller, double value) {
+        if (!repository.existsById(idDweller))
+            throw new NotFoundException();
+
+        repository.creditDweller(idDweller, value);
+    }
+
+    public void debit(int idDweller, double value) {
+        if (!repository.existsById(idDweller))
+            throw new NotFoundException();
+
+        repository.debitDweller(idDweller, value);
+    }
+
+    public double getBalance(int idDweller) {
+        if (!repository.existsById(idDweller))
+            throw new NotFoundException();
+
+        return repository.getBalance(idDweller);
+    }
+}
