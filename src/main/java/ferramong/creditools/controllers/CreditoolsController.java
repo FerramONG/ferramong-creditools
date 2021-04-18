ffa8d07449ee1a47a09f9112100cfd3f44e8e899
William Niemiec <williamniemiec@hotmail.com>
2021-04-18 01:54:04 -0300

    Doc

--- src/main/java/ferramong/creditools/controllers/CreditoolsController.java
+++ src/main/java/ferramong/creditools/controllers/CreditoolsController.java
@@ -17,6 +17,24 @@ public class CreditoolsController {
 
     private final CreditoolsService creditoolsService;
 
+    /**
+     * Credits some cash value to a dweller's account.
+     * (1 money unit <=> 10 creditools)
+     *
+     * <h2>CURL example</h2>
+     * <code>
+     *     curl "https://ferramong-creditools.herokuapp.com/wallet/credit/money" \
+     *     -X PUT \
+     *     -d "{\n  \"idDweller\": \"1\", \n  \"value\": \"99\"\n}" \
+     *     -H "Content-type: application/json"
+     * </code>
+     *
+     * @param       transaction Dweller id and value to be credited to the
+     * dweller's account
+     *
+     * @return      200 if ok; 404 if there is no dweller with the provided id;
+     * 400 if another error occurs
+     */
     @PutMapping("/wallet/credit/money")
     public void creditUsingMoney(@RequestBody TransactionInfo transaction) {
         creditoolsService.creditUsingMoney(
@@ -25,6 +43,23 @@ public class CreditoolsController {
         );
     }
 
+    /**
+     * Credits some creditool value to a dweller's account.
+     *
+     * <h2>CURL example</h2>
+     * <code>
+     *     curl "https://ferramong-creditools.herokuapp.com/wallet/credit/creditools" \
+     *     -X PUT \
+     *     -d "{\n  \"idDweller\": \"1\", \n  \"value\": \"99\"\n}" \
+     *     -H "Content-type: application/json"
+     * </code>
+     *
+     * @param       transaction Dweller id and value to be credited to the
+     * dweller's account
+     *
+     * @return      200 if ok; 404 if there is no dweller with the provided id;
+     * 400 if another error occurs
+     */
     @PutMapping("/wallet/credit/creditools")
     public void creditUsingCreditools(@RequestBody TransactionInfo transaction) {
         creditoolsService.creditUsingCreditools(
@@ -33,6 +68,23 @@ public class CreditoolsController {
         );
     }
 
+    /**
+     * Debits some creditool value to a dweller's account.
+     *
+     * <h2>CURL example</h2>
+     * <code>
+     *     curl "https://ferramong-creditools.herokuapp.com/wallet/debit" \
+     *     -X PUT \
+     *     -d "{\n  \"idDweller\": \"1\", \n  \"value\": \"9\"\n}" \
+     *     -H "Content-type: application/json"
+     * </code>
+     *
+     * @param       transaction Dweller id and value to be credited to the
+     * dweller's account
+     *
+     * @return      200 if ok; 404 if there is no dweller with the provided id;
+     * 400 if another error occurs
+     */
     @PutMapping("/wallet/debit")
     public void debit(@RequestBody TransactionInfo transaction) {
          creditoolsService.debit(
@@ -41,6 +93,19 @@ public class CreditoolsController {
          );
     }
 
+    /**
+     * Gets the balance from a dweller.
+     *
+     * <h2>CURL example</h2>
+     * <code>
+     *      curl "https://ferramong-creditools.herokuapp.com/wallet/dweller/1"
+     * </code>
+     *
+     * @param       idDweller Dweller id
+     *
+     * @return      Dweller balance and 200 if ok; 404 if there is no dweller
+     * with the provided id; 400 if another error occurs
+     */
     @GetMapping("/wallet/dweller/{id_dweller}")
     public ResponseEntity<Double> getBalance(@PathVariable("id_dweller") int idDweller) {
         return ResponseEntity.ok().body(creditoolsService.getBalance(idDweller));
